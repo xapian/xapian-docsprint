dddace2f24cd7754c81ef0cd7cf9f86ac68230f5
Olly Betts <olly@survex.com>
2011-11-06 12:16:04 +0000

    Fix whitespace; sync with updated Python comments.
    Use spaces for indentation rather than a mixture of spaces and tabs which
    assume non-standard 4 space tab setting.

--- code/php/index1.php
+++ code/php/index1.php
@@ -2,60 +2,62 @@
 require_once("xapian.php");
 require_once("parsecsv.php");
 
-// Start of example code
+// Start of example code.
 function index ($datapath, $dbpath)
 {
-	// Create or open the database we're going to be writing to. 
-	$db = new XapianWritableDatabase($dbpath, Xapian::DB_CREATE_OR_OPEN);
-	
-	// Set up a TermGenerator that we'll use in indexing
-	$termgenerator = new XapianTermGenerator();
-	$termgenerator->set_stemmer(new XapianStem('english'));
-	
-	// open the file
-	$fH = open_file($datapath);
-	
-	//	Read the header row in 
-	$headers = get_csv_headers($fH);
-	
-	while (($row = parse_csv_row($fH, $headers)) !== false) {
-        // mapping from field name to value using first row headers
-        // We're just going to use id_NUMBER, TITLE and DESCRIPTION
-		$description 	= $row['DESCRIPTION'];
-		$title 			= $row['TITLE'];
-		$identifier 	= $row['id_NUMBER'];
-		
-        // we make a document and tell the term generator to use this
+    // Create or open the database we're going to be writing to.
+    $db = new XapianWritableDatabase($dbpath, Xapian::DB_CREATE_OR_OPEN);
+
+    // Set up a TermGenerator that we'll use in indexing.
+    $termgenerator = new XapianTermGenerator();
+    $termgenerator->set_stemmer(new XapianStem('english'));
+
+    // Open the file.
+    $fH = open_file($datapath);
+
+    // Read the header row in.
+    $headers = get_csv_headers($fH);
+
+    while (($row = parse_csv_row($fH, $headers)) !== false) {
+        // '$row' maps field name to value.  The field names come from the
+        // first row of the CSV file.
+        //
+        // We're just going to use DESCRIPTION, TITLE and id_NUMBER.
+        $description = $row['DESCRIPTION'];
+        $title       = $row['TITLE'];
+        $identifier  = $row['id_NUMBER'];
+
+        // We make a document and tell the term generator to use this.
         $doc = new XapianDocument();
         $termgenerator->set_document($doc);
-		
-        // index each field with a suitable prefix
+
+        // Index each field with a suitable prefix.
         $termgenerator->index_text($title, 1, 'S');
         $termgenerator->index_text($description, 1, 'XD');
 
-        // index fields without prefixes for general search
+        // Index fields without prefixes for general search.
         $termgenerator->index_text($title);
         $termgenerator->increase_termpos();
         $termgenerator->index_text($description);
 
-        // store all the fields for display purposes
+        // Store all the fields for display purposes.
         $doc->set_data(json_encode($row));
 
-        // we use the identifier to ensure each object ends up
-        // in the database only once no matter how many times
-        // we run the indexer
+        // We use the identifier to ensure each object ends up in the
+        // database only once no matter how many times we run the
+        // indexer.
         $idterm = "Q".$identifier;
         $doc->add_term($idterm);
         $db->replace_document($idterm, $doc);
-	}
+    }
 }
-// End of example code
+// End of example code.
 
 if ($argc < 2) {
-	print "Usage: php index1.php <source.csv> <target_db_path>\n";
-	die();
+    print "Usage: php index1.php <source.csv> <target_db_path>\n";
+    die();
 }
 
-// call the index function
+// Call the index function.
 index($argv[1], $argv[2]);
 ?>
