e98918e3f030723341228a18b488e51e82f71ab4
Justin Finkelstein <justin@ilithium.com>
2011-11-06 11:16:21 +0000

    Added class for basic logging information

---
+++ code/php/logger.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Simple stdout logger; outputs the message to console
+ * 
+ * @param string $message Message to record to the log
+ * 
+ * @return void
+ */
+function log_info ($message)
+{
+	print "INFO:{$message}\n";
+}
+?>
