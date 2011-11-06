379885bcecceaf10d0e0858ba1322c44dce9b8d6
Justin Finkelstein <justin@ilithium.com>
2011-11-06 12:23:52 +0000

    Added filter index and search examples

---
+++ code/php/index_filters.php
@@ -0,0 +1,76 @@
+<?php
+require_once("xapian.php");
+require_once("parsecsv.php");
+
+// Start of example code
+function index ($datapath, $dbpath)
+{
+	// Create or open the database we're going to be writing to. 
+	$db = new XapianWritableDatabase($dbpath, Xapian::DB_CREATE_OR_OPEN);
+	
+	// Set up a TermGenerator that we'll use in indexing
+	$termgenerator = new XapianTermGenerator();
+	$termgenerator->set_stemmer(new XapianStem('english'));
+	
+	// open the file
+	$fH = open_file($datapath);
+	
+	//	Read the header row in 
+	$headers = get_csv_headers($fH);
+	
+	while (($row = parse_csv_row($fH, $headers)) !== false) {
+        // mapping from field name to value using first row headers
+        // We're just going to use id_NUMBER, TITLE and DESCRIPTION
+		$description 	= $row['DESCRIPTION'];
+		$title 			= $row['TITLE'];
+		$identifier 	= $row['id_NUMBER'];
+		$collection 	= $row['COLLECTION'];
+		$maker			= $row['MAKER'];
+		
+        // we make a document and tell the term generator to use this
+        $doc = new XapianDocument();
+        $termgenerator->set_document($doc);
+		
+        // index each field with a suitable prefix
+        $termgenerator->index_text($title, 1, 'S');
+        $termgenerator->index_text($description, 1, 'XD');
+
+        // index fields without prefixes for general search
+        $termgenerator->index_text($title);
+        $termgenerator->increase_termpos();
+        $termgenerator->index_text($description);
+
+        ### Start of new indexing code.
+        // index the MATERIALS field, splitting on semicolons
+        $materials = explode(";", $row['MATERIALS']);
+        foreach ($materials as $material)
+        {
+			$material = strtolower(trim($material));
+			if ($material != '')
+			{
+				$doc->add_boolean_term('XM'.$material);
+			}
+		}
+        ### End of new indexing code.
+        
+        // store all the fields for display purposes
+        $doc->set_data(json_encode($row));
+
+        // we use the identifier to ensure each object ends up
+        // in the database only once no matter how many times
+        // we run the indexer
+        $idterm = "Q".$identifier;
+        $doc->add_term($idterm);
+        $db->replace_document($idterm, $doc);
+	}
+}
+// End of example code
+
+if ($argc < 2) {
+	print "Usage: php index_facets.php <source.csv> <target_db_path>\n";
+	die();
+}
+
+// call the index function
+index($argv[1], $argv[2]);
+?>
---
+++ code/php/search_filters.php
@@ -0,0 +1,106 @@
+<?php
+require_once("xapian.php");
+require_once("parsecsv.php");
+require_once("logger.php");
+
+## Start of example code.
+function search($dbpath, $querystring, $materials, $offset = 0, $pagesize = 10)
+{
+	// offset - defines starting point within result set
+	// pagesize - defines number of records to retrieve
+	
+	// Open the database we're going to search.
+	$db = new XapianDatabase($dbpath);
+
+### Start of example code.
+    // Set up a QueryParser with a stemmer and suitable prefixes
+    $queryparser = new XapianQueryParser();
+    $queryparser->set_stemmer(new XapianStem("english"));
+    $queryparser->add_prefix("title", "S");
+    $queryparser->add_prefix("description", "XD");
+	
+    // And parse the query
+    $query = $queryparser->parse_query($querystring);
+
+	if (empty($materials) === false)
+	{
+        // Filter the results to ones which contain at least one of the
+		// materials.
+		$material_queries = array();
+	
+		// Build a query for each material value
+		foreach ($materials as $material)
+		{
+			$material = str_replace("material:", "", $material);
+			$material_queries[] = new XapianQuery('XM'.strtolower($material));
+		}
+		
+		// Combine these queries with an OR operator
+		$material_query = new XapianQuery(XapianQuery::OP_AND, $material_queries);
+		
+		// Use the material query to filter the main query
+		$query = new XapianQuery(XapianQuery::OP_FILTER, $query, $material_query);
+	}
+### End of example code.
+
+    // Use an Enquire object on the database to run the query
+    $enquire = new XapianEnquire($db);
+    $enquire->set_query($query);
+
+	// Set up a spy to inspect the MAKER value at slot 1
+	$spy = new XapianValueCountMatchSpy(1);
+	$enquire->add_matchspy($spy);
+
+	// Retrieve the matches and compute start and end points
+	$matches = $enquire->get_mset($offset, $pagesize);
+	$start = $matches->begin();
+	$end = $matches->end();
+	$index = 0;
+	
+	// Use an array to record the DocIds of each match
+	$docids = array();
+
+	while (!($start->equals($end)))
+	{
+		// retrieve the document and its data
+		$doc = $start->get_document();
+		$fields = json_decode($doc->get_data());
+		$position = $offset + $index + 1;
+
+		// record the docid
+		$docid = $start->get_docid();
+		$docids[] = $docid;
+	
+		// display the results
+		print sprintf("%d: #%03d %s\n", $position, $docid, $fields->TITLE);
+
+		// increment MSet iterator and our counter
+		$start->next();
+		$index++;
+	}
+
+	// Parse and display the spy values
+	$spy_start = $spy->values_begin();
+	$spy_end = $spy->values_end();
+	
+	while (!($spy_start->equals($spy_end)))
+	{
+		print sprintf("Facet: %s; count: %d\n",
+			$spy_start->get_term(),
+			$spy_start->get_termfreq()
+		);
+		
+		$spy_start->next();
+	}
+	
+	// Finally, make sure we log the query and displayed results
+	log_info(sprintf("xapian.search:'%s'[%d:%d] = %s",
+				$querystring,
+				$offset,
+				$offset+$pagesize,
+				implode(" ", $docids)
+			));
+}
+
+search($argv[1], $argv[2], array_slice($argv, 3));
+?>
