7a2159f064b6a842e079d0231c06b9407acebba3
Dan Colish <dcolish@gmail.com>
2012-10-22 11:24:37 -0700

    Fix encoding for practical_example, fix role

--- code/python/index1.py
+++ code/python/index1.py
@@ -36,7 +36,7 @@ def index(datapath, dbpath):
         termgenerator.index_text(description)
 
         # Store all the fields for display purposes.
-        doc.set_data(json.dumps(fields, ensure_ascii=False))
+        doc.set_data(json.dumps(fields, encoding='latin1'))
 
         # We use the identifier to ensure each object ends up in the
         # database only once no matter how many times we run the
