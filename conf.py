302220e3ecc7ff32d008aec866e1c274b109fd8d
James Aylett <james@tartarus.org>
2014-10-27 17:33:22 -0700

    Soft indentation, not tab characters.

--- conf.py
+++ conf.py
@@ -505,7 +505,7 @@ def xapian_check_examples():
             sys.stdout.flush()
             print "vimdiff %s %s" % (tmp_out, filename)
             if os.system("diff -u %s %s 2>&1" % (tmp_out, filename)) == 0:
-		os.unlink(tmp_out)
+                os.unlink(tmp_out)
 
     if bad:
         raise SystemExit()
