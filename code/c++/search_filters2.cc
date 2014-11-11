157bcddd84ae7e319a740212be1119f1f170f475
Olly Betts <olly@survex.com>
2014-11-12 09:32:16 +1300

    translate search_filters2 to C++

---
+++ code/c++/search_filters2.cc
@@ -0,0 +1,73 @@
+#include <xapian.h>
+
+#include <cstdio>
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "support.h"
+
+using namespace std;
+
+static void
+search(const string & dbpath, const string & querystring,
+       Xapian::doccount offset = 0, Xapian::doccount pagesize = 10)
+{
+    // offset - defines starting point within result set.
+    // pagesize - defines number of records to retrieve.
+
+    // Open the database we're going to search.
+    Xapian::Database db(dbpath);
+
+// Start of example code.
+    // Set up a QueryParser with a stemmer and suitable prefixes.
+    Xapian::QueryParser queryparser;
+    queryparser.set_stemmer(Xapian::Stem("en"));
+    queryparser.set_stemming_strategy(queryparser.STEM_SOME);
+    queryparser.add_prefix("title", "S");
+    queryparser.add_prefix("description", "XD");
+    queryparser.add_boolean_prefix("material", "XM");
+
+    // And parse the query.
+    Xapian::Query query = queryparser.parse_query(querystring);
+// End of example code.
+
+    // Use an Enquire object on the database to run the query.
+    Xapian::Enquire enquire(db);
+    enquire.set_query(query);
+
+    // And print out something about each match.
+    Xapian::MSet mset = enquire.get_mset(offset, pagesize);
+
+    clog << "'" << querystring << "'[" << offset << ":" << offset + pagesize
+	 << "] =";
+    for (Xapian::MSetIterator m = mset.begin(); m != mset.end(); ++m) {
+	Xapian::docid did = *m;
+	cout << m.get_rank() + 1 << ": #" << setfill('0') << setw(3) << did
+	     << ' ';
+
+	const string & data = m.get_document().get_data();
+	cout << get_field(data, 1) << endl;
+	// Log the document id.
+	clog << ' ' << did;
+    }
+    clog << endl;
+}
+
+int main(int argc, char** argv) {
+    if (argc < 3) {
+	cerr << "Usage: " << argv[0] << " DBPATH QUERYTERM..." << endl;
+	return 1;
+    }
+    const char * dbpath = argv[1];
+
+    // Join the rest of the arguments with spaces to make the query string.
+    string querystring;
+    for (argv += 2; *argv; ++argv) {
+	if (!querystring.empty()) querystring += ' ';
+	querystring += *argv;
+    }
+
+    search(dbpath, querystring);
+}
