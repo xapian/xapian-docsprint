858b88e994047fd09c781b531716a4ee0c72494b
James Aylett <james@tartarus.org>
2016-03-23 19:05:19 +0000

    Update copyright date to 2016.

--- conf.py
+++ conf.py
@@ -56,7 +56,7 @@ release = '1.2.19'
 _project = u'Getting Started with Xapian'
 _authors = u'Xapian Documentation Team & Contributors'
 project = u'%s %s' % ( _project, version)
-copyright = u'2003-2015 ' + _authors
+copyright = u'2003-2016 ' + _authors
 
 github_project_url = 'https://github.com/xapian/xapian-docsprint/blob/master'
 
