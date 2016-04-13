8ef3f5ad9e7ab4072e93f57a8c1fd9d986f82e39
Aakash Muttineni <aakash.muttineni@gmail.com>
2016-04-13 18:17:31 +0530

    CSV parsing
    Modified parseCsvLine function in support.java to ignore quotes of the csv values while parsing.
    Also when double quotes "" are present within a quoted csv value, those are changed to single quotes in the output.

--- code/java/support.java
+++ code/java/support.java
@@ -4,21 +4,33 @@ package code.java;
 import java.util.ArrayList;
 
 public class support {
-
 	// Returns an ArrayList of the parsed CSV line
 	public static ArrayList<String> parseCsvLine(String csvLine) {
 		ArrayList<String> words = new ArrayList<String>();
-		boolean notInsideComma = true;int start = 0, end = 0;
+		boolean insideQuote = false, endEarly = false;
+		int start = 0, end = 0;
 		for (int i = 0; i < csvLine.length()-1; i++) {
-			if(csvLine.charAt(i) == ',' && notInsideComma) {
-				words.add(csvLine.substring(start,i));
-				start = i + 1;
+			if(csvLine.charAt(i) == ',' && !insideQuote) {
+				if (endEarly) {
+					words.add(csvLine.substring(start,i-1).replace("\"\"","\""));
+					endEarly = false;
+				} else {
+					words.add(csvLine.substring(start,i));
+				}
+
+				if(csvLine.charAt(i+1) == '"') {
+					start = i + 2;
+					i++;
+					endEarly = true;
+					insideQuote = true;
+				} else {
+					start = i + 1;
+				}
 			} else if (csvLine.charAt(i) == '"') {
-				notInsideComma =! notInsideComma;
+				insideQuote = !insideQuote;
 			}
 		}
 		words.add(csvLine.substring(start));
 		return words;
 	}
-
 }
