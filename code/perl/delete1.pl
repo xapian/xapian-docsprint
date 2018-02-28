af2dd0d44e03e3d03d7c347d9b5eea23f971b8eb
Marco Pessotto <melmothx@gmail.com>
2018-02-28 10:59:55 +0100

    Ported delete1.py to perl

---
+++ code/perl/delete1.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Search::Xapian ':all';
+
+my ($db_path, @ids) = @ARGV;
+die "Usage $0 DBPATH ID..." unless $db_path && @ids;
+
+delete_docs($db_path, @ids);
+
+sub delete_docs {
+    my ($db_path, @ids) = @_;
+    my $db = Search::Xapian::WritableDatabase->new($db_path, DB_CREATE_OR_OPEN);
+    foreach my $id (@ids) {
+        $db->delete_document_by_term("Q$id");
+    }
+}
+
