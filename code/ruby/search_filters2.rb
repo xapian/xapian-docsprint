1e0f73048b5e0055f31ae292021875878d48f5ba
Marco Pessotto <marco@pessotto.hr>
2023-04-02 08:36:39 +0200

    Implement search_filters2.rb

---
+++ code/ruby/search_filters2.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'xapian'
+require 'json'
+require_relative 'support'
+
+def search(dbpath, querystring, offset: 0, pagesize: 10)
+  # offset - defines starting point within result set
+  # pagesize - defines number of records to retrieve
+
+  # Open the database we're going to search.
+  db = Xapian::Database.new(dbpath)
+
+### Start of example code.
+  # Set up a QueryParser with a stemmer and suitable prefixes
+  queryparser = Xapian::QueryParser.new
+  queryparser.stemmer = Xapian::Stem.new('en')
+  queryparser.stemming_strategy = Xapian::QueryParser::STEM_SOME
+  queryparser.add_prefix('title', 'S')
+  queryparser.add_prefix('description', 'XD')
+  queryparser.add_boolean_prefix('material', 'XM')
+
+  # And parse the query
+  query = queryparser.parse_query(querystring)
+### End of example code.
+
+  # Use an Enquire object on the database to run the query
+  enquire = Xapian::Enquire.new(db)
+  enquire.query = query
+
+  # And print out something about each match
+  matches = []
+  enquire.mset(offset, pagesize).matches.each do |match|
+    fields = JSON.parse(match.document.data)
+    printf "%<rank>i: #%<docid>3.3i %<title>s\n",
+           rank: match.rank + 1,
+           docid: match.docid,
+           title: fields['TITLE']
+    matches << match.docid
+  end
+  log_matches(querystring, offset, pagesize, matches)
+end
+### End of example code.
+
+abort "Usage #{__FILE__} DBPATH QUERY..." if ARGV.length < 2
+
+search(ARGV[0], ARGV[1], ARGV[2..])
