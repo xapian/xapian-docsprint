4c4c474f96d605db1851f62158d1dcff7f96a2dd
Marco Pessotto <marco@pessotto.hr>
2023-04-24 11:09:50 +0200

    Linting with rubocop

--- code/ruby/support.rb
+++ code/ruby/support.rb
@@ -8,41 +8,35 @@ def parse_csv_file(datapath)
 end
 
 def parse_states(datapath)
-  CSV.read(datapath, headers: true).select {|r| r['order'] }
+  CSV.read(datapath, headers: true).select { |r| r['order'] }
 end
 
 def log_matches(querystring, offset, pagesize, matches)
   puts "'#{querystring}'[#{offset}:#{offset + pagesize}] = #{matches.join(' ')}"
 end
 
-def numbers_from_string(s)
+def numbers_from_string(string)
   out = []
-  s.scan(/[\d.]*\d[\d.]*/) do |n|
+  string.scan(/[\d.]*\d[\d.]*/) do |n|
     out << n.to_f
   end
-  return out
+  out
 end
 
 def format_numeral(numeral, sep: ',')
-  if numeral.is_a? Integer
-    _numeral = []
-    numeral.to_s.split('').reverse.each_with_index do |s,i|
-      if i > 0 and i % 3 == 0 and i != numeral.to_s.size
-        _numeral << sep
-      end
-      _numeral << s
-    end
-    return _numeral.reverse.join('')
-  else
-    raise "Numeral must be an int type to format"
+  raise 'Numeral must be an int type to format' unless numeral.is_a?(Integer)
+
+  out = []
+  numeral.to_s.split('').reverse.each_with_index do |s, i|
+    out << sep if i.positive? && (i % 3).zero? && i != numeral.to_s.size
+    out << s
   end
+  out.reverse.join('')
 end
 
 def format_date(datestr)
-  if datestr.is_a? String
-    _date = DateTime.strptime(datestr, '%Y%m%d')
-    return "#{_date.strftime('%B')} #{_date.day}, #{_date.year}"
-  else
-    raise "Could not parse date to format 'YYYYMMDD'"
-  end
+  raise "Could not parse date to format 'YYYYMMDD'" unless datestr.is_a? String
+
+  date = DateTime.strptime(datestr, '%Y%m%d')
+  "#{date.strftime('%B')} #{date.day}, #{date.year}"
 end
