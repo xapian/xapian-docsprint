76d752507b5d59ae6423c294e67b49f99900d3e8
Olly Betts <olly@survex.com>
2023-04-28 11:15:25 +1200

    Fix warning about README.rst
    This is essentially meta-documentation, not part of the guide
    itself, so it's expected that this isn't in any toctree.

--- conf.py
+++ conf.py
@@ -95,7 +95,7 @@ github_project_url = 'https://github.com/xapian/xapian-docsprint/blob/master'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', 'attic', 'language_specific']
+exclude_patterns = ['README.rst', '_build', 'attic', 'language_specific']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
