xapian/xapian-docsprint

commit 439eb27af8bd99b614fc2be7ad2cac4d7449b7d4

Drop python2 support
We're no longer able to CI test with on GHA, and Xapian 2.0.0 has
dropped support for it.

Realistically anyone still using Xapian with Python2 at this point is
maintaining an existing legacy application and very averse to change,
so probably not someone likely to be reading this guide anyway.

Makefile
@@ -30,13 +30,13 @@ help:
 	@echo
 	@echo "By default, the '$(default_language)' version is built.  To specify a language, use e.g.:"
 	@echo "  make <target> LANGUAGE=c++"
-	@echo "Known languages: `python conf.py --list-languages`"
+	@echo "Known languages: `python3 conf.py --list-languages`"
 
 clean:
 	-rm -rf $(BUILDDIR)/*
 	-rm -rf db statesdb
 	-rm -rf code/c++/built
-	-rm -f code/python/*.pyc code/python3/*.pyc
+	-rm -f code/ython3/*.pyc
 
 html:
 	$(SPHINXBUILD) -b html $(ALLSPHINXOPTS) $(BUILDDIR)/html

.github/workflows/ci.yml
@@ -69,25 +69,6 @@ jobs:
     - name: make html
       run: make html LANGUAGE=perl
 
-  python2:
-    if: false
-    runs-on: 'ubuntu-20.04'
-    steps:
-    - name: Check out repository code
-      uses: actions/checkout@v3
-    - name: Install CCache
-      uses: hendrikmuhs/ccache-action@v1
-      with:
-        key: python2
-    - name: Install package dependencies
-      run: |
-        sudo apt-get update
-        sudo apt-get install \
-            python3-sphinx \
-            python-xapian
-    - name: make html
-      run: make html LANGUAGE=python
-
   python3:
     runs-on: 'ubuntu-22.04'
     steps:

code/python/delete1.py
@@ -1,20 +1,0 @@
-#!/usr/bin/env python
-
-import sys
-import xapian
-
-### Start of example code.
-def delete_docs(dbpath, identifiers):
-    # Open the database we're going to be deleting from.
-    db = xapian.WritableDatabase(dbpath, xapian.DB_OPEN)
-
-    for identifier in identifiers:
-        idterm = u'Q' + identifier
-        db.delete_document(idterm)
-### End of example code.
-
-if len(sys.argv) < 3:
-    print("Usage: %s DBPATH ID..." % sys.argv[0])
-    sys.exit(1)
-
-delete_docs(dbpath = sys.argv[1], identifiers=sys.argv[2:])

code/python/index1.py
@@ -1,52 +1,0 @@
-#!/usr/bin/env python
-
-import json
-import sys
-import xapian
-from support import parse_csv_file
-
-### Start of example code.
-def index(datapath, dbpath):
-    # Create or open the database we're going to be writing to.
-    db = xapian.WritableDatabase(dbpath, xapian.DB_CREATE_OR_OPEN)
-
-    # Set up a TermGenerator that we'll use in indexing.
-    termgenerator = xapian.TermGenerator()
-    termgenerator.set_stemmer(xapian.Stem("en"))
-
-    for fields in parse_csv_file(datapath):
-        # 'fields' is a dictionary mapping from field name to value.
-        # Pick out the fields we're going to index.
-        description = fields.get('DESCRIPTION', u'')
-        title = fields.get('TITLE', u'')
-        identifier = fields.get('id_NUMBER', u'')
-
-        # We make a document and tell the term generator to use this.
-        doc = xapian.Document()
-        termgenerator.set_document(doc)
-
-        # Index each field with a suitable prefix.
-        termgenerator.index_text(title, 1, 'S')
-        termgenerator.index_text(description, 1, 'XD')
-
-        # Index fields without prefixes for general search.
-        termgenerator.index_text(title)
-        termgenerator.increase_termpos()
-        termgenerator.index_text(description)
-
-        # Store all the fields for display purposes.
-        doc.set_data(json.dumps(fields))
-
-        # We use the identifier to ensure each object ends up in the
-        # database only once no matter how many times we run the
-        # indexer.
-        idterm = u"Q" + identifier
-        doc.add_boolean_term(idterm)
-        db.replace_document(idterm, doc)
-### End of example code.
-
-if len(sys.argv) != 3:
-    print("Usage: %s DATAPATH DBPATH" % sys.argv[0])
-    sys.exit(1)
-
-index(datapath = sys.argv[1], dbpath = sys.argv[2])

code/python/index_facets.py
@@ -1,60 +1,0 @@
-#!/usr/bin/env python
-
-import json
-import sys
-import xapian
-from support import parse_csv_file
-
-### Start of example code.
-def index(datapath, dbpath):
-    # Create or open the database we're going to be writing to.
-    db = xapian.WritableDatabase(dbpath, xapian.DB_CREATE_OR_OPEN)
-
-    # Set up a TermGenerator that we'll use in indexing.
-    termgenerator = xapian.TermGenerator()
-    termgenerator.set_stemmer(xapian.Stem("en"))
-
-    for fields in parse_csv_file(datapath):
-        # 'fields' is a dictionary mapping from field name to value.
-        # Pick out the fields we're going to index.
-        description = fields.get('DESCRIPTION', u'')
-        title = fields.get('TITLE', u'')
-        identifier = fields.get('id_NUMBER', u'')
-        collection = fields.get('COLLECTION', u'')
-        maker = fields.get('MAKER', u'')
-
-        # We make a document and tell the term generator to use this.
-        doc = xapian.Document()
-        termgenerator.set_document(doc)
-
-        # Index each field with a suitable prefix.
-        termgenerator.index_text(title, 1, 'S')
-        termgenerator.index_text(description, 1, 'XD')
-
-        # Index fields without prefixes for general search.
-        termgenerator.index_text(title)
-        termgenerator.increase_termpos()
-        termgenerator.index_text(description)
-
-        # Add the collection as a value in slot 0.
-        doc.add_value(0, collection)
-
-        # Add the maker as a value in slot 1.
-        doc.add_value(1, maker)
-
-        # Store all the fields for display purposes.
-        doc.set_data(json.dumps(fields))
-
-        # We use the identifier to ensure each object ends up in the
-        # database only once no matter how many times we run the
-        # indexer.
-        idterm = u"Q" + identifier
-        doc.add_boolean_term(idterm)
-        db.replace_document(idterm, doc)
-### End of example code.
-
-if len(sys.argv) != 3:
-    print("Usage: %s DATAPATH DBPATH" % sys.argv[0])
-    sys.exit(1)
-
-index(datapath = sys.argv[1], dbpath = sys.argv[2])

code/python/index_filters.py
@@ -1,58 +1,0 @@
-#!/usr/bin/env python
-
-import json
-import sys
-import xapian
-from support import parse_csv_file
-
-def index(datapath, dbpath):
-    # Create or open the database we're going to be writing to.
-    db = xapian.WritableDatabase(dbpath, xapian.DB_CREATE_OR_OPEN)
-
-    # Set up a TermGenerator that we'll use in indexing.
-    termgenerator = xapian.TermGenerator()
-    termgenerator.set_stemmer(xapian.Stem("en"))
-
-    for fields in parse_csv_file(datapath):
-        # 'fields' is a dictionary mapping from field name to value.
-        # Pick out the fields we're going to index.
-        description = fields.get('DESCRIPTION', u'')
-        title = fields.get('TITLE', u'')
-        identifier = fields.get('id_NUMBER', u'')
-
-        # We make a document and tell the term generator to use this.
-        doc = xapian.Document()
-        termgenerator.set_document(doc)
-
-        # Index each field with a suitable prefix.
-        termgenerator.index_text(title, 1, 'S')
-        termgenerator.index_text(description, 1, 'XD')
-
-        # Index fields without prefixes for general search.
-        termgenerator.index_text(title)
-        termgenerator.increase_termpos()
-        termgenerator.index_text(description)
-
-        ### Start of new indexing code.
-        # Index the MATERIALS field, splitting on semicolons.
-        for material in fields.get('MATERIALS', u'').split(';'):
-            material = material.strip().lower()
-            if len(material) > 0:
-                doc.add_boolean_term('XM' + material)
-        ### End of new indexing code.
-
-        # Store all the fields for display purposes.
-        doc.set_data(json.dumps(fields))
-
-        # We use the identifier to ensure each object ends up in the
-        # database only once no matter how many times we run the
-        # indexer.
-        idterm = u"Q" + identifier
-        doc.add_boolean_term(idterm)
-        db.replace_document(idterm, doc)
-
-if len(sys.argv) != 3:
-    print("Usage: %s DATAPATH DBPATH" % sys.argv[0])
-    sys.exit(1)
-
-index(datapath = sys.argv[1], dbpath = sys.argv[2])

code/python/index_ranges.py
@@ -1,64 +1,0 @@
-#!/usr/bin/env python
-
-import json
-import sys
-import xapian
-from support import numbers_from_string, parse_csv_file
-
-def index(datapath, dbpath):
-    # Create or open the database we're going to be writing to.
-    db = xapian.WritableDatabase(dbpath, xapian.DB_CREATE_OR_OPEN)
-
-    # Set up a TermGenerator that we'll use in indexing.
-    termgenerator = xapian.TermGenerator()
-    termgenerator.set_stemmer(xapian.Stem("en"))
-
-    for fields in parse_csv_file(datapath):
-        # 'fields' is a dictionary mapping from field name to value.
-        # Pick out the fields we're going to index.
-        description = fields.get('DESCRIPTION', u'')
-        title = fields.get('TITLE', u'')
-        identifier = fields.get('id_NUMBER', u'')
-
-        # We make a document and tell the term generator to use this.
-        doc = xapian.Document()
-        termgenerator.set_document(doc)
-
-        # Index each field with a suitable prefix.
-        termgenerator.index_text(title, 1, 'S')
-        termgenerator.index_text(description, 1, 'XD')
-
-        # Index fields without prefixes for general search.
-        termgenerator.index_text(title)
-        termgenerator.increase_termpos()
-        termgenerator.index_text(description)
-
-        # Store all the fields for display purposes.
-        doc.set_data(json.dumps(fields))
-
-### Start of example code.
-        # parse the two values we need
-        measurements = fields.get('MEASUREMENTS', u'')
-        if len(measurements) > 0:
-            numbers = numbers_from_string(measurements)
-            if len(numbers) > 0:
-                doc.add_value(0, xapian.sortable_serialise(max(numbers)))
-
-        date_made = fields.get('DATE_MADE', u'')
-        years = numbers_from_string(date_made)
-        if len(years) > 0:
-            doc.add_value(1, xapian.sortable_serialise(years[0]))
-### End of example code.
-
-        # We use the identifier to ensure each object ends up in the
-        # database only once no matter how many times we run the
-        # indexer.
-        idterm = u"Q" + identifier
-        doc.add_boolean_term(idterm)
-        db.replace_document(idterm, doc)
-
-if len(sys.argv) != 3:
-    print("Usage: %s DATAPATH DBPATH" % sys.argv[0])
-    sys.exit(1)
-
-index(datapath = sys.argv[1], dbpath = sys.argv[2])

code/python/index_ranges2.py
@@ -1,65 +1,0 @@
-#!/usr/bin/env python
-
-import json
-from support import parse_states
-import sys
-import xapian
-
-def index(datapath, dbpath):
-    # Create or open the database we're going to be writing to.
-    db = xapian.WritableDatabase(dbpath, xapian.DB_CREATE_OR_OPEN)
-
-    # Set up a TermGenerator that we'll use in indexing.
-    termgenerator = xapian.TermGenerator()
-    termgenerator.set_stemmer(xapian.Stem("en"))
-
-    for fields in parse_states(datapath):
-        # 'fields' is a dictionary mapping from field name to value.
-        # Pick out the fields we're going to index.
-        name = fields.get('name', u'')
-        description = fields.get('description', u'')
-        motto = fields.get('motto', u'')
-        admitted = fields.get('admitted', None)
-        population = fields.get('population', None)
-        order = fields.get('order', u'')
-
-        # We make a document and tell the term generator to use this.
-        doc = xapian.Document()
-        termgenerator.set_document(doc)
-
-### Start of example code.
-        # Index each field with a suitable prefix.
-        termgenerator.index_text(name, 1, 'S')
-        termgenerator.index_text(description, 1, 'XD')
-        termgenerator.index_text(motto, 1, 'XM')
-
-        # Index fields without prefixes for general search.
-        termgenerator.index_text(name)
-        termgenerator.increase_termpos()
-        termgenerator.index_text(description)
-        termgenerator.increase_termpos()
-        termgenerator.index_text(motto)
-
-        # Add document values.
-        if admitted is not None:
-            doc.add_value(1, xapian.sortable_serialise(int(admitted[:4])))
-            doc.add_value(2, admitted) # YYYYMMDD
-        if population is not None:
-            doc.add_value(3, xapian.sortable_serialise(int(population)))
-### End of example code.
-
-        # Store all the fields for display purposes.
-        doc.set_data(json.dumps(fields))
-
-        # We use the order to ensure each object ends up in the
-        # database only once no matter how many times we run the
-        # indexer.
-        idterm = u"Q" + order
-        doc.add_boolean_term(idterm)
-        db.replace_document(idterm, doc)
-
-if len(sys.argv) != 3:
-    print("Usage: %s DATAPATH DBPATH" % sys.argv[0])
-    sys.exit(1)
-
-index(datapath = sys.argv[1], dbpath = sys.argv[2])

commit 103cfd2db730ec6b78b79f95cf3f52221266ed32

Fix cleaning of .pyc files
Typo introduced by 439eb27af8bd99b614fc2be7ad2cac4d7449b7d4

Makefile
@@ -36,7 +36,7 @@ clean:
 	-rm -rf $(BUILDDIR)/*
 	-rm -rf db statesdb
 	-rm -rf code/c++/built
-	-rm -f code/ython3/*.pyc
+	-rm -f code/python3/*.pyc
 
 html:
 	$(SPHINXBUILD) -b html $(ALLSPHINXOPTS) $(BUILDDIR)/html